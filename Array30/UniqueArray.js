286c4da2dca5976c2497d5cba3450db573872a71
Viplav jha <viplavjha1993@gmail.com>
2023-10-06 13:28:14 +0530

    unique array py

--- Array30/UniqueArray.js
+++ Array30/UniqueArray.js
@@ -1,17 +1,50 @@
 // Input: arr[] = {12, 10, 9, 45, 2, 10, 10, 45}
 // Output: 12, 10, 9, 45, 2
 
-function UniqueArray(arr){
- let n =arr.length
- let newArray =[]
- for(i=0;i<n;i++){
-    if(newArray.indexOf(arr[i])===-1){
-        newArray.push(arr[i])
-    }
- }
- return newArray;
+// function UniqueArray(arr){
+//  let n =arr.length
+//  let newArray =[]
+//  for(i=0;i<n;i++){
+//     if(newArray.indexOf(arr[i])===-1){
+//         newArray.push(arr[i])
+//     }
+//  }
+//  return newArray;
    
-}
-const input =[12, 10, 9, 45, 2, 10, 10, 45]
-const result =UniqueArray(input)
-console.log(result);
+// }
+// const input =[12, 10, 9, 45, 2, 10, 10, 45]
+// const result =UniqueArray(input)
+// console.log(result);
+
+function UniqueArray(arr){
+    let duplicate =[];
+    let numCount ={};
+   
+    for(let num of arr) {
+        numCount[num] =(numCount[num] || 0) + 1;
+        if( numCount[num]===2){
+            duplicate.push(num)
+        }
+    }
+    return duplicate
+   }
+   const input =[12, 10, 9, 45, 2, 10, 10, 45]
+   const result =UniqueArray(input)
+   console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Array30/uniqueArray.py
@@ -0,0 +1,13 @@
+# // Input: arr[] = {12, 10, 9, 45, 2, 10, 10, 45}
+# // Output: 12, 10, 9, 45, 2
+
+def removeDuplicate(arr):
+    unique_list =[]
+    for item in arr:
+        if item not in unique_list:
+           unique_list.append(item)
+    return unique_list
+    
+input =[1,1,3,3,3,5,6,7]
+result=removeDuplicate(input)
+print(result)
