45a924f1ba129fefdfb70584e5c84a46524e05ff
Viplav jha <viplavjha1993@gmail.com>
2023-10-10 13:40:20 +0530

    greater element from neighbour

---
+++ Array30/neighbourArray.js
@@ -0,0 +1,17 @@
+// Input: array[]= {5, 10, 20, 15}
+// Output: 20
+// Explanation: The element 20 has neighbors 10 and 15, both of them are less than 20.
+
+function neighbourArray(arr) {
+ let newArr =[]
+ let n=arr.length
+ for(let i=0;i<n;i++)
+  if(arr[i]>arr[i+1] && arr[i] >arr[i-1]){
+    newArr.push(arr[i])
+  }
+  return newArr
+}
+
+const input =[5, 10, 20, 15]
+const result =neighbourArray(input) 
+console.log(result)
