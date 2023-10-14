cbfc1d6519a0e1928bde71b76cf05f324ce556e1
Viplav jha <viplavjha1993@gmail.com>
2023-10-14 12:48:26 +0530

    python reverse

---
+++ Array30/String/Reverse.py
@@ -0,0 +1,13 @@
+
+s = "i love programming very much"
+
+# Split the string into words
+words = s.split()
+
+# Reverse the list of words
+words.reverse()
+
+# Join the reversed words back into a string
+s = " ".join(words)
+
+print(s)
