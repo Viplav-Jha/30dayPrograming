06b7006212baf06d94c27421d23c218186b6b97e
Viplav jha <viplavjha1993@gmail.com>
2023-10-20 21:58:18 +0530

    logics

--- Array30/String/Closest.js
+++ Array30/String/Closest.js
@@ -6,10 +6,35 @@
 // Output: 2
 // Explanation: Minimum distance between the words “geeks” and “practice” is 2
 
-function Closest(){
+function findMinDistance(words, word1, word2) {
+    let index1 = -1; // Initialize with an invalid index
+    let index2 = -1; // Initialize with an invalid index
+    let minDistance = Infinity;
 
+    for (let i = 0; i < words.length; i++) {
+        if (words[i] === word1) {
+            index1 = i;
+        } else if (words[i] === word2) {
+            index2 = i;
+        }
+
+        if (index1 !== -1 && index2 !== -1) {
+            const distance = Math.abs(index1 - index2);
+            if (distance < minDistance) {
+                minDistance = distance;
+            }
+        }
+    }
+
+    return minDistance;
 }
 
-const input ="{ “the”, “quick”, “brown”, “fox”, “quick”}, word1 = “the”, word2 = “fox” "
-const result=Closest(input);
-console.log(input)
+const words1 = ["the", "quick", "brown", "fox", "quick"];
+const word1_1 = "the";
+const word2_1 = "fox";
+console.log(findMinDistance(words1, word1_1, word2_1)); // Output: 3
+
+const words2 = ["geeks", "for", "geeks", "contribute", "practice"];
+const word1_2 = "geeks";
+const word2_2 = "practice";
+console.log(findMinDistance(words2, word1_2, word2_2)); // Output: 2
