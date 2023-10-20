6aa13ada6227e72fdf91b9e6db1db96f25e9df14
Viplav jha <viplavjha1993@gmail.com>
2023-10-20 21:24:27 +0530

    closest string

---
+++ Array30/String/Closest.js
@@ -0,0 +1,15 @@
+// Input: S = { “the”, “quick”, “brown”, “fox”, “quick”}, word1 = “the”, word2 = “fox”
+// Output: 3
+// Explanation: Minimum distance between the words “the” and “fox” is 3
+
+// Input: S = {“geeks”, “for”, “geeks”, “contribute”,  “practice”}, word1 = “geeks”, word2 = “practice”
+// Output: 2
+// Explanation: Minimum distance between the words “geeks” and “practice” is 2
+
+function Closest(){
+
+}
+
+const input ="{ “the”, “quick”, “brown”, “fox”, “quick”}, word1 = “the”, word2 = “fox” "
+const result=Closest(input);
+console.log(input)
