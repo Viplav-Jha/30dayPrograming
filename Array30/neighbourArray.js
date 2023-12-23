1996f837a47fffd12e0876b97771a4fae3db22d6
Viplav jha <viplavjha1993@gmail.com>
2023-12-23 19:39:30 +0530

    old question practice

--- Array30/neighbourArray.js
+++ Array30/neighbourArray.js
@@ -3,15 +3,15 @@
 // Explanation: The element 20 has neighbors 10 and 15, both of them are less than 20.
 
 function neighbourArray(arr) {
- let newArr =[]
- let n=arr.length
- for(let i=0;i<n;i++)
-  if(arr[i]>arr[i+1] && arr[i] >arr[i-1]){
-    newArr.push(arr[i])
-  }
-  return newArr
+  let newArr = [];
+  let n = arr.length;
+  for (let i = 0; i < n; i++)
+    if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
+      newArr.push(arr[i]);
+    }
+  return newArr;
 }
 
-const input =[5, 10, 20, 15]
-const result =neighbourArray(input) 
-console.log(result)
+const input = [5, 10, 20, 15];
+const result = neighbourArray(input);
+console.log(result);
