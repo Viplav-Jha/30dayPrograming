07b81a2482a77497eca9e16334c31bc319a2cf8f
Viplav jha <viplavjha1993@gmail.com>
2024-01-02 12:08:33 +0530

    kth frquency array

--- Array30/180days/kthFrequenceArray.js
+++ Array30/180days/kthFrequenceArray.js
@@ -1,17 +1,18 @@
-function mostOccurence(arr,k){
- let newArr =[];
- let n= arr.length;
-  for(let i=0;i<n;i++){
-    const element=arr[i];
-    const occurence= arr.filter((el)=>el===element).length
+function mostOccurence(arr, k) {
+  let newArr = [];
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    const element = arr[i];
+    const occurence = arr.filter((el) => el === element).length;
 
-    if(occurence ===k && newArr.indexOf(element) ===-1){
-        newArr.push(element)
+    if (occurence === k && newArr.indexOf(element) === -1) {
+      newArr.push(element);
     }
   }
-  return newArr
+  return newArr;
 }
 
-const input =[1,1,1,2,2,3]
-const k=3;nce(input,k)
-console.log(result)
+const input = [1, 1, 1, 2, 2, 3];
+const k = 3;
+nce(input, k);
+console.log(result);
