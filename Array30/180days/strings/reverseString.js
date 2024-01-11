5386fef6c9f464de502be30c9841ccec2ec83e9c
Viplav jha <viplavjha1993@gmail.com>
2024-01-11 20:52:13 +0530

    some correction in code reverse logics

--- Array30/180days/strings/reverseString.js
+++ Array30/180days/strings/reverseString.js
@@ -1,20 +1,20 @@
-// function reverseString(str){
-//   const reverseStr = str.split('').reverse().join(" ")
-//   return reverseStr;
-// }
-
-// const input = "My name is viplav"
-// const output =reverseString(input);
-// console.log(output)
-
 function reverseString(str){
-     let reverseStr = '';
-     for(let i =str.length-1; i>=0 ;i--){
-        reverseStr += str[i];
-     }
-     return reverseStr
-  }
   
-  const input = "My name is viplav"
-  const output =reverseString(input);
-  console.log(output)
+  return str.split('').reverse().join("")
+}
+
+const input = "Hello World!!"
+const output =reverseString(input);
+console.log(output)
+
+// function reverseString(str){
+//      let reverseStr = '';
+//      for(let i =str.length-1; i>=0 ;i--){
+//         reverseStr += str[i];
+//      }
+//      return reverseStr
+//   }
+  
+//   const input = "My name is viplav"
+//   const output =reverseString(input);
+//   console.log(output)
