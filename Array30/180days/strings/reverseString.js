89c2e3ec69726a9720da6a7e4ceba5b77065d36c
Viplav jha <viplavjha1993@gmail.com>
2024-01-11 20:49:06 +0530

    reverse string without build method

--- Array30/180days/strings/reverseString.js
+++ Array30/180days/strings/reverseString.js
@@ -1,8 +1,20 @@
-function reverseString(str){
-  const reverseStr = str.split(' ').reverse().join(" ")
-  return reverseStr;
-}
+// function reverseString(str){
+//   const reverseStr = str.split('').reverse().join(" ")
+//   return reverseStr;
+// }
 
-const input = "My name is viplav"
-const output =reverseString(input);
-console.log(output)
+// const input = "My name is viplav"
+// const output =reverseString(input);
+// console.log(output)
+
+function reverseString(str){
+     let reverseStr = '';
+     for(let i =str.length-1; i>=0 ;i--){
+        reverseStr += str[i];
+     }
+     return reverseStr
+  }
+  
+  const input = "My name is viplav"
+  const output =reverseString(input);
+  console.log(output)
