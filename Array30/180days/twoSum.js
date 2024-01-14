d6c35ca230f0a6a5c34a6683e2ee8773e89f0490
Viplav jha <viplavjha1993@gmail.com>
2024-01-14 20:21:11 +0530

    twoSum

--- Array30/180days/twoSum.js
+++ Array30/180days/twoSum.js
@@ -3,21 +3,19 @@
 // // The indices of the elements 2 and 7 add up to the target 9.
 // // Therefore, the expected output is [0, 1].
 
-
-function twoSum(arr,target){
-    const n=arr.length
-    for(let i=0;i<n;i++){
-        for(let j=1;j<n;j++){
-            if(arr[i]+arr[j] ===target){
-                return [i,j]
-            }
-        }
+function twoSum(arr, target) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 1; j < n; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [i, j];
+      }
     }
-    return null
-    
+  }
+  return null;
 }
 
-const input = [2, 11,7, 15];
-const target=9;
-const output =twoSum(input,target);
-console.log(output)
+const input = [2, 11, 7, 15];
+const target = 9;
+const output = twoSum(input, target);
+console.log(output);
