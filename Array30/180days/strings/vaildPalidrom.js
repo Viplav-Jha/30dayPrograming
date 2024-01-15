1fe17de926aa8b37279c67a433341325e1a0b2a2
Viplav jha <viplavjha1993@gmail.com>
2024-01-15 15:29:39 +0530

    vaild palidrom string

---
+++ Array30/180days/strings/vaildPalidrom.js
@@ -0,0 +1,13 @@
+function isPalindrome(str) {
+    // Your code here
+    const cleanStr = str.replace('[^a-zA-Z0-9]/g','').toLowerCase()
+    console.log(cleanStr)
+    const reverseStr = cleanStr.split('').reverse().join('')
+    console.log(reverseStr)
+     return cleanStr === reverseStr
+  }
+  
+  console.log(isPalindrome("A man, a plan, a canal, Panama!")); // Should output true
+  console.log(isPalindrome("race a car")); // Should output false
+  console.log(isPalindrome("Madam, in Eden, I'm Adam")); // Should output true
+  
