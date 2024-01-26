19c05224f558dc0fc2b97176f8aebd792498280b
Viplav jha <viplavjha1993@gmail.com>
2024-01-26 22:30:42 +0530

    26th jan

--- Array30/180days/REVISE_QUESTION/uniqueAaary.js
+++ Array30/180days/REVISE_QUESTION/uniqueAaary.js
@@ -1,20 +1,18 @@
 function uniqueArray(nums) {
-  if(nums.length <=1){
+  if (nums.length <= 1) {
     return nums;
   }
-  let uniqueIndex =0;
-  for(let i=1;i<nums.length; i++){
-    if(nums[i]!==nums[uniqueIndex]){
-        uniqueIndex++;
-        nums[uniqueIndex] =nums[i];
-       
-       
+  let uniqueIndex = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[uniqueIndex]) {
+      uniqueIndex++;
+      nums[uniqueIndex] = nums[i];
     }
   }
-  nums.length =uniqueIndex+1;
+  nums.length = uniqueIndex + 1;
   return nums;
 }
 
-const input = [1,2,2,3,3,4,4,7,]
-const output=uniqueArray(input);
+const input = [1, 2, 2, 3, 3, 4, 4, 7];
+const output = uniqueArray(input);
 console.log(output);
