9244672100f2c5a4bb59c0cef20aa98f37721f3c
Viplav jha <viplavjha1993@gmail.com>
2024-01-28 21:38:39 +0530

    indentation changes

--- Array30/180days/REVISE_QUESTION/DSA_ALL_Qestion/maxNumber.js
+++ Array30/180days/REVISE_QUESTION/DSA_ALL_Qestion/maxNumber.js
@@ -1,13 +1,11 @@
-function maxNumber(nums){
- const n= nums.length;
- if(n===0){
-    return `invalid array`
- }
+function maxNumber(nums) {
+  const n = nums.length;
+  if (n === 0) {
+    return `invalid array`;
+  }
 
- return Math.max(...nums)
-
+  return Math.max(...nums);
 }
 const input = [10, 2, 12, 10, 3, 5, 12];
 const output = maxNumber(input);
 console.log(output);
-
